7bb8f7b45e518498f6ba091cd7379ffd55cea9fc
Tim Englart <timenglartdev@gmail.com>
2021-10-11 21:56:18 +1000

    add basic drv8825 functions
    - not fully implemented

--- drv8825.c
+++ drv8825.c
@@ -1,14 +1,28 @@
-struct stepper_motor {
-    void (*update)();
-    char title[50];
-};
+#include "drv8825.h"
+#include "pico.h"
 
-void b() {
-
+void drv_set_enabled(uint8_t gpio_pin, bool enabled)
+{
+    sleep_us(1);
 }
-void a() {
-    struct stepper_motor smx;
-    if(smx.title == "s") {
-        smx.update();
-    }
+
+float drv_determine_step(bool mode_0, bool mode_1, bool mode_2)
+{
+    if(!mode_0 && !mode_1 && !mode_2) 
+        return 1; // Full step (2-phase excitation) with 71% current
+    else if(!mode_0 && !mode_1 && mode_2)
+        return 0.5; // 1/2 step (1-2 phase excitation)
+    else if(!mode_0 && mode_1 && !mode_2)
+        return 0.25; // 1/4 step (W1-2 phase excitation)
+    else if(!mode_0 && mode_1 && mode_2)
+        return 0.125; // 8 microsteps/step
+    else if(mode_0 && !mode_1 && !mode_2)
+        return 0.0625; // 16 microsteps/step
+    else
+        return 0.03125; // 32 microsteps/step
+}
+
+float drv_determine_distance(float step_amount, unsigned int n_steps)
+{
+    return step_amount * n_steps;
 }
--- drv8825.h
+++ drv8825.h
@@ -1,36 +1,39 @@
 #ifndef DRV8825_H
 #define DRV8825_H
 
-enum DRV8825_PINS {
-    CP1 = 1,
-    CP2,
-    VCP,
-    VMA,
-    A_OUT_1,
-    ISENA,
-    A_OUT_2,
-    B_OUT_2,
-    ISENB,
-    B_OUT_1, 
-    VMB,
-    AVREF,
-    BVREF,
-    GND_14,
-    V3P3OUT,
-    nRESET,
-    nSLEEP,
-    nFAULT,
-    DECAY,
-    DIR,
-    nENBL,
-    STEP,
-    NC,
-    MODE_0,
-    MODE_1,
-    MODE_2,
-    nHome,
-    GND_28
-};
+#include <stdbool.h>
 
+// TODO: Implement all declared functions
+//      - Refactor if necessary 
+
+// Data Sheet: https://www.ti.com/lit/gpn/drv8825
+
+
+/*
+    Timing Requirements:            MIN	MAX	UNIT
+    1	ƒSTEP	Step frequency		250	kHz
+    2	tWH(STEP)	Pulse duration, STEP high	1.9		μs
+    3	tWL(STEP)	Pulse duration, STEP low	1.9		μs
+    4	tSU(STEP)	Setup time, command before STEP rising	650		ns
+    5	tH(STEP)	Hold time, command after STEP rising	650		ns
+    6	tENBL	Enable time, nENBL active to STEP	650		ns
+    7	tWAKE	Wakeup time, nSLEEP inactive high to STEP input accepted		1.7	ms
+*/
+
+#define DRV_STEP_FREQ_SLEEP 5
+#define DRV_STEP_DURATION 3
+#define DRV_SETUP_TIME_DURATION 3
+
+typedef unsigned char uint8_t;
+
+
+
+void drv_set_enabled(uint8_t gpio_pin, bool enabled);
+char drv_has_fault(uint8_t gpio_pin);
+void drv_reset(uint8_t gpio_pin, bool reset_state);
+void drv_sleep(uint8_t gpio_pin, bool sleep_state);
+
+float drv_determine_step(bool mode_0, bool mode_1, bool mode_2);
+float drv_determine_distance(float step_amount, unsigned int n_steps);
 
 #endif // DRV8825_H
